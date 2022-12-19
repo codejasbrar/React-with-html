0427557184961107ec0ff4f4f0e233f9ea5d96b8
codejasbrar <jasbrar585@gmail.com>
2022-12-19 19:35:33 +0000

    React app

---
+++ src/pages/About.js
@@ -0,0 +1,31 @@
+import './About.css';
+const About = () => {
+return (
+	<div>
+	
+	<h1>About Page</h1>
+    <div className="container">
+      <div className="image">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" />
+      </div>
+      <div className="text">
+        <h1>This is my experiment in React JS</h1>
+	    <p>I am using HTML in React</p>
+      </div>
+      </div>
+      <div className="container">
+      <div className="text">
+        <h1>This is my experiment in React JS</h1>
+	    <p>I am using HTML in React</p>
+      </div>
+      <div className="image">
+        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" />
+      </div>
+    </div>
+    
+	</div>
+    
+);
+};
+
+export default About;
