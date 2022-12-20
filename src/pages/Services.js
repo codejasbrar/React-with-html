25470d6dc4d3ecee400d8df34c55ba0bff689620
codejasbrar <jasbrar585@gmail.com>
2022-12-20 05:01:19 +0000

    services grid links

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -1,27 +1,58 @@
+import {BrowserRouter as Router, Link} from 'react-router-dom';
 import './Services.css';
 const Services = () => {
 return (
-	<div>
-	 <h1>Services Page</h1>
-    <div className="container">
-      <div className="image">
-        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" />
-      </div>
-      <div className="text">
-        <h1>This is my experiment in React JS</h1>
-	    <p>I am using HTML in React</p>
-      </div>
-      </div>
-      <div className="container">
-      <div className="text">
-        <h1>This is my experiment in React JS</h1>
-	    <p>I am using HTML in React</p>
-      </div>
-      <div className="image">
-        <img src="https://i.pinimg.com/564x/59/32/29/593229739184504afd9507cc42a9cb86.jpg" />
-      </div>
+	 <div className="grid services">
+  <article>
+    <img src="https://www.quackit.com/pix/samples/23m.jpg" alt="Sample photo"/>
+    <div className="text">
+      <h3>Seamlessly visualize quality</h3>
+      <p>Collaboratively administrate empowered markets via plug-and-play networks.</p>
+      <Link to="/About">
+      <button>Here's why</button>
+      </Link>
     </div>
-    
+  </article>
+  <article>
+    <img src="https://www.quackit.com/pix/samples/24m.jpg" alt="Sample photo"/>
+    <div className="text">
+      <h3>Completely Synergize</h3>
+      <p>Dramatically engage seamlessly visualize quality intellectual capital without superior collaboration and idea-sharing.</p>
+      <Link to="/About">
+      <button>Here's how</button>
+      </Link>
+    </div>
+  </article>
+  <article>
+    <img src="https://www.quackit.com/pix/samples/22l.jpg" alt="Sample photo"/>
+    <div className="text">
+      <h3>Dynamically Procrastinate</h3>
+      <p>Completely synergize resource taxing relationships via premier niche markets.</p>
+      <Link to="/About">
+        <button>Read more</button>
+        </Link>
+    </div>
+  </article>
+  <article>
+    <img src="https://www.quackit.com/pix/samples/15l.jpg" alt="Sample photo"/>
+    <div className="text">
+      <h3>Best in class</h3>
+      <p>Imagine jumping into that boat, and just letting it sail wherever the wind takes you...</p>
+      <Link to="/About">
+      <button>Just do it...</button>
+      </Link>
+    </div>
+  </article>
+  <article>
+    <img src="https://www.quackit.com/pix/samples/25m.jpg" alt="Sample photo"/>
+    <div className="text">
+      <h3>Dynamically innovate supply chains</h3>
+      <p>Holisticly predominate extensible testing procedures for reliable supply chains.</p>
+      <Link to="/About">
+      <button>Here's why</button>
+      </Link>
+    </div>
+  </article>
 	</div>
     
 );
